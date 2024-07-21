2194f29d4df2f06e3dc615fe04d15f49105266a3
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-21 19:23:20 +0530

    ..

--- server.js
+++ server.js
@@ -28,7 +28,15 @@ const corsOptions = {
   accessControlAllowCredentials: true
 };
 
+
+
+
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 // cron.schedule('*/15 * * * *', () => {
