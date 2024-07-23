5404d792869ef3957db608beeed95e6d6643dad6
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-23 20:32:09 +0530

    cors

--- server.js
+++ server.js
@@ -14,6 +14,36 @@ const PORT = process.env.PORT || 3000;
 
 const  app = express();
 
+
+
+
+
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+cron.schedule('*/15 * * * *', () => {
+  // Ping your app's URL to keep it active
+  axios.get('https://lets-chat-backend-7s3j.onrender.com');
+});
+
+
+
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://localhost:5173");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+
+
+
+
 const corsOptions = {
   origin: (origin, callback) => {
     if (origin === 'http://localhost:5173' || origin === 'https://chat-react-app-frontend.onrender.com') {
@@ -32,46 +62,6 @@ const corsOptions = {
 
 
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
-// cron.schedule('*/15 * * * *', () => {
-//   // Ping your app's URL to keep it active
-//   axios.get('https://lets-chat-backend-7s3j.onrender.com');
-// });
-
-
-
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://localhost:5173");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-
-// app.use(cors(corsOptions));
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || origin === 'http://localhost:5173') {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: ['Content-Type']
-}));
-
-
-
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
