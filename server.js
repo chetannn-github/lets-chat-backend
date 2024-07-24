380cc39df91b37ffe26012960dd9b62373651c16
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-24 14:56:47 +0530

    cors

--- server.js
+++ server.js
@@ -19,13 +19,18 @@ const  app = express();
 
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://lets-chat-frontend.onrender.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://lets-chat-frontend.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    res.status(200).send();
+  } else {
+    next();
+  }
+});
 
 cron.schedule('*/15 * * * *', () => {
   // Ping your app's URL to keep it active
