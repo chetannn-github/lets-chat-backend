5bbb22ab2868e2a9df39c5c1ebd0e8b6a6644519
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-25 12:49:48 +0530

    socket

--- socket/socket.js
+++ socket/socket.js
@@ -1,82 +1,76 @@
-// // import express from "express"
-// // import http, { Server } from "http";
+// import express from "express"
+// import http, { Server } from "http";
 
-
-// // const app = express();
-// // const server = http.createServer(app);
-
-// // const io = new Server (server,{
-// //     cors:{
-// //         origin:["http://localhost:5173"],
-// //         methods:["GET","POST"]
-// //     }
-// // });
-
-// // io.on("connection",(socket)=>{
-// //     console.log(socket.id);
-  
-// //     socket.on("disconnect",()=>{
-// //         console.log("user disconnected",socket.id)
-// //     })
-// //   })
-
-// // export {io,app,server}
-
-
-// import express from 'express';
-// import http from 'http';
-// import { Server } from 'socket.io';
 
 // const app = express();
+// const server = http.createServer(app);
 
-// // const userSocketMap = {};
+// const io = new Server (server,{
+//     cors:{
+//         origin:["http://localhost:5173"],
+//         methods:["GET","POST"]
+//     }
+// });
 
-// // const server = http.createServer(app);
-// // const io = new Server(server, {
-// //   cors: {
-// //     origin: ['https://localhost:8134'],
-// //     methods: ['GET', 'POST'],
-// //     allowedHeaders: ['Content-Type'],
-// //     credentials: true,
-   
-// // }  });
+// io.on("connection",(socket)=>{
+//     console.log(socket.id);
+  
+//     socket.on("disconnect",()=>{
+//         console.log("user disconnected",socket.id)
+//     })
+//   })
 
-
-// // export const   getRecieverSocketId  = (recieverId) =>{
-// // return userSocketMap[recieverId]
-// // }
-
-
-
-// // io.on('connection', (socket) => {
-// //   console.log("hii " , socket.id)
+// export {io,app,server}
 
 
-// //   const userId = socket.handshake.query.userId;
-// // console.log(userId)
-// //   if(userId) {
-// //     userSocketMap[userId] = socket.id;
-// //   }
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+
+const userSocketMap = {};
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors:{
+    origin: 'https://lets-chat-frontend.onrender.com',
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With'],
+    credentials: true,
+    sameSite: 'none',
+  }  });
+
+
+export const   getRecieverSocketId  = (recieverId) =>{
+return userSocketMap[recieverId]
+}
 
 
 
-// //   io.emit("getOnlineUsers",Object.keys(userSocketMap));
+io.on('connection', (socket) => {
+  console.log("hii " , socket.id)
 
 
-// //   socket.on("disconnect",()=>{
-// //     console.log("disconnected" , socket.id);
-// //     delete userSocketMap[userId];
-// //      io.emit("getOnlineUsers",Object.keys(userSocketMap));
-// //   })
+     const userId = socket.handshake.query.userId;
+    console.log(userId)
+    if(userId) {userSocketMap[userId] = socket.id;}
+
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+
+    socket.on("disconnect",()=>{
+    console.log("disconnected" , socket.id);
+    delete userSocketMap[userId];
+     io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    })
   
   
+  });
 
 
-// //   });
 
 
-
-// // // Export io, app, and server
-// export { io, app, server };
+export { io, app, server };
 
 
