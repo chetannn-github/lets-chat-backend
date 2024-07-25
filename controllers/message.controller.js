64756ea1aa9cdb691bdad1da6110280bac7b850e
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-25 13:21:45 +0530

    socket

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -16,6 +16,7 @@ export const sendMessage = async(req,res) =>{
         participants:{$all:[senderId,recieverId]}
     });
 
+    
     if(!conversation){
         conversation = new Conversation({
             participants:[senderId,recieverId],
