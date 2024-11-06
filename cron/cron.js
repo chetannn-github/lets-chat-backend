ffeaac171e8f70cfae96ea1ec0f3a848d3b68ed9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2024-11-06 19:28:35 +0530

    Update cron.js

--- cron/cron.js
+++ cron/cron.js
@@ -1,20 +1,20 @@
-import cron from "cron";
-import https from "https";
+// import cron from "cron";
+// import https from "https";
 
-const URL = "https://lets-chat-backend-7s3j.onrender.com/api/test";
+// const URL = "https://lets-chat-backend-7s3j.onrender.com/api/test";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
-	https
-		.get(URL, (res) => {
-			if (res.statusCode === 200) {
-				console.log("GET request sent successfully");
-			} else {
-				console.log("GET request failed", res.statusCode);
-			}
-		})
-		.on("error", (e) => {
-			console.error("Error while sending request", e);
-		});
-});
+// const job = new cron.CronJob("*/14 * * * *", function () {
+// 	https
+// 		.get(URL, (res) => {
+// 			if (res.statusCode === 200) {
+// 				console.log("GET request sent successfully");
+// 			} else {
+// 				console.log("GET request failed", res.statusCode);
+// 			}
+// 		})
+// 		.on("error", (e) => {
+// 			console.error("Error while sending request", e);
+// 		});
+// });
 
-export default job;
+// export default job;
